25148f71e4ca13c87c6374c35509932b4922f540
Frank <franksun418@gmail.com>
2018-05-22 17:44:47 -0700

    EOD_5.22

---
+++ Assets/Scripts/BoxCommands.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxCommands : MonoBehaviour {
+
+    PistonsController PC = GameObject.FindObjectOfType<PistonsController>();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnSelect() {
+        PC.collectivey += 0.1f;
+    }
+}
