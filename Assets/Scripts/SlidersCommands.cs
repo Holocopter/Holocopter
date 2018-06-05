be2ed140a76b524ff9944e138f413e5d552e7209
Xiangpeng Hao <3150102255@zju.edu.cn>
2018-06-04 23:38:23 -0700

    :beer: animate on slider value change

--- Assets/Scripts/SlidersCommands.cs
+++ Assets/Scripts/SlidersCommands.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SlidersCommands : MonoBehaviour {
+public class SlidersCommands : MonoBehaviour
+{
     PistonsController pc;
     GameObject SpeedSlider;
     GameObject CollectiveSlider;
@@ -11,9 +13,16 @@ public class SlidersCommands : MonoBehaviour {
     GameObject MainCamera;
     public Slider speedSlider;
     public Slider collectiveSlider;
+
     public Slider sizeSlider;
-	// Use this for initialization
-	void Start () {
+
+    private float sizeTargetValue = 0.0f;
+    private float sizeCurValue = 0.0f;
+    private bool animiDone = true;
+
+    // Use this for initialization
+    void Start()
+    {
         pc = GameObject.FindObjectOfType<PistonsController>();
         SpeedSlider = GameObject.Find("SpeedSlider");
         speedSlider = SpeedSlider.GetComponent<Slider>();
@@ -21,18 +30,38 @@ public class SlidersCommands : MonoBehaviour {
         collectiveSlider = CollectiveSlider.GetComponent<Slider>();
         SizeSlier = GameObject.Find("SizeSlider");
         sizeSlider = SizeSlier.GetComponent<Slider>();
+        sizeSlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
         MainCamera = GameObject.Find("MixedRealityCameraParent");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        pc.sp = speedSlider.value;
-        pc.collectivey = collectiveSlider.value;
-        MainCamera.transform.position = new Vector3(MainCamera.transform.position.x, MainCamera.transform.position.y, sizeSlider.value);
-        
+
+        sizeTargetValue = sizeCurValue = sizeSlider.value;
     }
 
-    void Speed_slider() {
-       
+    // Update is called once per frame
+    void Update()
+    {
+        pc.sp = speedSlider.value;
+        pc.collectivey = collectiveSlider.value;
+        MainCamera.transform.position = new Vector3(MainCamera.transform.position.x, MainCamera.transform.position.y,
+            sizeSlider.value);
+
+        sizeCurValue = Mathf.MoveTowards(sizeCurValue, sizeTargetValue, Time.deltaTime * 1);
+        sizeSlider.value = sizeCurValue;
+        if (Math.Abs(sizeCurValue - sizeTargetValue) < 0.01)
+        {
+            animiDone = true;
+        }
+    }
+
+    public void ValueChangeCheck()
+    {
+        if (animiDone)
+        {
+            sizeTargetValue = sizeSlider.value;
+            animiDone = false;
+        }
+    }
+
+    void Speed_slider()
+    {
     }
 }
