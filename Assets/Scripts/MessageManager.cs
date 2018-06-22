cd18af37137efcb83f26de5b18eec4e3f51e25c7
Xiangpeng Hao <3150102255@zju.edu.cn>
2018-06-22 14:34:58 -0700

    :beer: Guess what I did?

--- Assets/Scripts/MessageManager.cs
+++ Assets/Scripts/MessageManager.cs
@@ -28,7 +28,6 @@ public class MessageManager : Singleton<MessageManager>
     {
         DebugMsg = HoloToolkit.Sharing.MessageID.UserMessageIDStart,
         ChangeSize,
-        Smaller,
         Max
     }
 
