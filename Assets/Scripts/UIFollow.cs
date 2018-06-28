ae643b3d8d100e42d847bfb9a2be0be9890fd2b2
Xiangpeng Hao <3150102255@zju.edu.cn>
2018-06-28 14:12:34 -0700

    :bug: fix

---
+++ Assets/Scripts/UIFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFollow : MonoBehaviour {
+    public Transform user;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(user.position.x, transform.position.y, user.position.z);
+	}
+}
