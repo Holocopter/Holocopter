115e4fbde52e3c29f9e3be2052e0ce0ec2720d3f
Frank <franksun418@gmail.com>
2018-07-09 17:01:44 -0700

    :beer: added belt UI animated

--- Assets/Scripts/UIFollow.cs
+++ Assets/Scripts/UIFollow.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class UIFollow : MonoBehaviour {
     public Transform user;
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Transform target;
+    Quaternion initialRotation;
+    // Use this for initialization
+    void Start () {
+        //store initial rotation
+       initialRotation = Quaternion.Euler(new Vector3(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y, transform.localRotation.eulerAngles.z));
+    }
 	
 	// Update is called once per frame
 	void Update () {
         transform.position = new Vector3(user.position.x, transform.position.y, user.position.z);
-	}
+        transform.LookAt(target);
+    }
 }
